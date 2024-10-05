993791014f1b0db3e05b7ff1d58a1a517a7a4373
IgorKha <2268209+IgorKha@users.noreply.github.com>
2024-10-05 15:21:56 +0500

    feat: Add 404.html for handling page not found errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: HomeView
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
