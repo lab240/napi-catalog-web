1619556a9bb881a611a1e208ee37ad527d697c6f
IgorKha <2268209+IgorKha@users.noreply.github.com>
2024-10-05 15:35:07 +0500

    refactor: Update router history mode to use hash instead of history
    This commit updates the router history mode in the `index.ts` file to use hash instead of history. This change was made to address an issue with page not found errors when deploying the website. The `createWebHashHistory` function is now used instead of `createWebHistory`.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SensorDetail from '@/views/sensor/SensorDetail.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/:pathMatch(.*)*',
